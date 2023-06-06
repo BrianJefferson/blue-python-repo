4202ab37a6dc2f3ef475a661e5612fa1a34581b9
Brian Jefferson <intellectsociety@hotmail.com>
2023-06-06 15:22:37 +0000

    random number generator

---
+++ 06-06-2023 practice.py
@@ -0,0 +1,13 @@
+# 6/6/2023 Practice Playground
+
+import random
+
+number = random.randint(0, 10)
+
+print(number)
+
+if number > 7:
+    print('Big number')
+elif number < 7:
+    print('small number')
+    
